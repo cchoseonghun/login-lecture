dca4a6029d0281388dfd0f45804439486563fc77
seonghun <cchoseonghun@gmail.com>
2022-10-15 14:43:34 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,11 +12,22 @@ const id = document.querySelector('#id'),
             psword: psword.value
         };
 
+        console.log('test: ' + req);
+
         fetch('/login', {
             method: 'POST', 
-            header: {
-                'Content-Type': 'application/json'
+            headers: {
+                'Content-Type': 'application/json', 
             }, 
             body: JSON.stringify(req)
         })
+        // console.log로 찍어보면 Promise가 반환됨
+        .then((res) => res.json()) 
+        // Promise는 then을 통해 접근할 수 있음
+        // .then((res) => console.log(res));  
+        // parameter로 넘기는 값을 어떠한 함수 안에 다시 parameter로 넘길 때 생략가능
+        // .then(console.log);  // 이렇게
+        .then((res) => {
+
+        });
     }

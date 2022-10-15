b93b0193647d3084e5da1ddfadaed839c3c4c322
seonghun <cchoseonghun@gmail.com>
2022-10-15 22:48:31 +0900

    User 모델 구현 - 복습필요

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
     home: (req, res)=>{
@@ -13,33 +13,10 @@ const output = {
 
 const process = {
     login: (req, res)=>{
-        // console.log(req.body);
-        const id = req.body.id, 
-            psword = req.body.psword;
-
-        // console.log(UserStorage.users); // UserStorage 내부 users가 static이어야함
-        // console.log(UserStorage.getUsers('id', 'psword'));
-        const users = UserStorage.getUsers('id', 'psword');
-
-        const response = {};
-        if((users.id.includes(id))){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-                // return res.json({
-                //     success: true, 
-                // });
-            }
-        }
-
-        response.success = false;
-        response.msg = '로그인에 실패하셨습니다.';
+        const user = new User((req.body));
+        const response = user.login();
+        // console.log(response);
         return res.json(response);
-        // return res.json({
-        //     success: false, 
-        //     msg: '로그인에 실패하셨습니다.', 
-        // })
     }, 
 };
 
